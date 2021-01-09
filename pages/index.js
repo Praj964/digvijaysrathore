3d5f2bb04bffd4f96a155436cf0c24a3acb004c2
Digvijay Singh Rathore <dynamicdigvijay@gmail.com>
2021-01-09 15:28:03 +0530

    home pg

--- pages/index.js
+++ pages/index.js
@@ -26,12 +26,17 @@ export default function Index() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Layout pageTitle="Digvijay - Hacker" description="My Personal Blog">
+      <Layout pageTitle="Digvijay — Developer, Maker" description="My Personal Blog">
+
+
         <div className="hero-div">
+          <h1 style={{
+            fontWeight: "900px"
+          }}>Hey, I'm Digvijay!</h1>
           <p style={{
             fontSize: "18px",
             lineHeight: "1.6"
-          }}>Hey! I am Digvijay. I write code, design UX and build products. I am interested in tech, startups, humans, evolution & philosophy and would love to have a chat around that. I have been sharing my notes at <Link href="/posts"><span style={InlineLinksStyle}>/posts</span></Link>. Below is what I'm spending most of my time at.</p>
+          }}>I write code, design UX and build products. I am interested in tech, startups, humans, evolution & philosophy and would love to have a chat around that. I have been sharing my notes at <Link href="/posts"><span style={InlineLinksStyle}>/posts</span></Link>. Below is what I'm spending most of my time at.</p>
         </div>
 
         <ul style={{
@@ -46,6 +51,8 @@ export default function Index() {
           fontSize: "18px",
           lineHeight: "1.6"
         }}>Know more about what I am upto at <Link href="/now"><span style={InlineLinksStyle}>/now</span></Link>.</p>
+
+        <h1>Random</h1>
 
         <p style={{
           fontSize: "18px",
