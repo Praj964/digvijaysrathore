fd09329b7b68e5b2ddd2dbf67b6b74467e750605
Digvijay Singh Rathore <dynamicdigvijay@gmail.com>
2021-02-01 21:50:08 +0530

    index page

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ export default function Index() {
   const InlineLinksStyle = {
     backgroundColor: mode ? "#F1F1F1" : "#1A1A1A",
     color: "#000000",
-    backgroundColor: "#f3f169"
+    backgroundColor: "#f3f169",
+    textDecoration: "none"
   }
 
   return (
@@ -29,32 +30,32 @@ export default function Index() {
       <Layout pageTitle="Digvijay — Developer, Maker" description="My Personal Blog">
 
 
-        <div className="hero-div">
+        <div className="main-div main-text">
           <p style={{
-            fontSize: "18px",
+            fontSize: "26px",
             lineHeight: "1.6"
-          }}>Hey! I'm <a href="https://twitter.com/novadigvijay" target="_blank" rel="noopener"><span style={InlineLinksStyle}>Digvijay</span></a>. I write code, design UX and build products. Currently, I am working on <a href="https://kotaru.io" target="_blank" rel="noopener"><span style={InlineLinksStyle}>Kotaru</span></a>, a platform to share your personal notes with ease. Browse through my <a href="#projects"><span style={InlineLinksStyle}>projects</span></a> mentioned below.
+          }}>> Hey! I'm <a href="https://twitter.com/novadigvijay" target="_blank" rel="noopener"><span style={InlineLinksStyle}>Digvijay</span></a>. I write code, design UX and build products. Currently, I am working on <a href="https://kotaru.io" target="_blank" rel="noopener"><span style={InlineLinksStyle}>Kotaru</span></a>, a platform to share your personal notes with ease. Browse through my <a href="#projects"><span style={InlineLinksStyle}>projects</span></a> mentioned below.
           </p>
-          <p style={{
-            fontSize: "18px",
-            lineHeight: "1.6"
-          }}>
-          I am interested in tech, startups, humans, evolution & philosophy and would love to have a chat around that. I have been sharing my notes at <Link href="/posts"><span style={InlineLinksStyle}>/posts</span></Link>.
-          </p>
-          <p style={{
-            fontSize: "18px",
+          <p className="main-text" style={{
+            fontSize: "26px",
             lineHeight: "1.6"
           }}>
-          Visit <Link href="/now"><span style={InlineLinksStyle}>/now</span></Link> to know more about what I am upto and <Link href="/random"><span style={InlineLinksStyle}>/random</span></Link> to read some random facts about me.
+          > I am interested in tech, startups, humans, evolution & philosophy and would love to have a chat around that. I have been sharing my notes at <Link href="/posts"><span style={InlineLinksStyle}>/posts</span></Link>.
           </p>
+          {/* <p style={{
+            fontSize: "26px",
+            lineHeight: "1.6"
+          }}>
+          > Visit <Link href="/now"><span style={InlineLinksStyle}>/now</span></Link> to know more about what I am upto and <Link href="/random"><span style={InlineLinksStyle}>/random</span></Link> to read some random facts about me.
+          </p> */}
         </div>
 
-        <p style={{
-          fontSize: "18px"
-        }}>By the way,</p>
+        <p className="main-text" style={{
+          fontSize: "26px"
+        }}>> By the way,</p>
 
-        <ul style={{
-          fontSize: "18px",
+        <ul className="main-text" style={{
+          fontSize: "26px",
           lineHeight: "1.8"
         }}>
           <li>I won the <a target="_blank" rel="noopener" href="https://hackinout.co"><span style={InlineLinksStyle}>InOut 7.0</span></a> by Devfolio.</li>
@@ -62,10 +63,10 @@ export default function Index() {
           <li>My article got featured on <a target="_blank" rel="noopener" href="https://hashnode.com"><span style={InlineLinksStyle}>Hashnode</span></a>'s must read list.</li>
         </ul>
 
-        <div style={{padding: 10}} />
+        {/* <div style={{padding: 10}} />
         <div id="projects">
           <Projects />
-        </div>
+        </div> */}
         <div style={{padding: 50}} />
       </Layout>
     </div>
